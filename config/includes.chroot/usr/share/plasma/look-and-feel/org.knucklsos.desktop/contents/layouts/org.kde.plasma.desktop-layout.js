ad78da1fc1c1437fe292ad13862d532ef018c898
Shaun <shaun@knucklsos.local>
2026-08-29 20:50:42 +0000

    Fix branding via proper mechanism: apply KnucklsOS look-and-feel as default (wallpaper+FolderView+green), rebuild plymouth initramfs with fist, installer icon on desktop

---
+++ config/includes.chroot/usr/share/plasma/look-and-feel/org.knucklsos.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,14 @@
+[Containments][1]
+activityId=1
+formfactor=desktop
+location=0
+plugin=org.kde.desktopcontainment
+wallpaperplugin=org.kde.image
+Icon=start-here-kde
+
+[Containments][1][General]
+ToolBoxButtonState=1
+Show Desktop Icons=true
+
+[Containments][1][Wallpaper][org.kde.image][General]
+Image=file:///usr/share/knucklsos/assets/knuckls-wordmark-os.jpg
